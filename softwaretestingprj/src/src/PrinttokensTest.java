3c764141052274f6c76799a863cfdb27848d0c13
victoriabauer <vic.ebauer@gmail.com>
2025-04-26 23:12:47 -0500

    method:3,4,7,8,9,10,11

--- softwaretestingprj/src/src/PrinttokensTest.java
+++ softwaretestingprj/src/src/PrinttokensTest.java
@@ -1,144 +1,218 @@
 import org.junit.jupiter.api.Test;
 
+import java.io.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrinttokensTest {
 
     @org.junit.jupiter.api.Test
     void open_character_stream() {
-        Printtokens pt = new Printtokens();
-        BufferedReader br = pt.open_character_stream(null);
-        assertNotNull(br, "BufferedReader should not be null when input is null");
+
     }
 
     @org.junit.jupiter.api.Test
     void get_char() {
-        Printtokens pt = new Printtokens();
-        BufferedReader br = pt.open_character_stream(null);
-        assertFalse(Printtokens.get_char(null), "Should print out the statement given");
-        assertTrue(Printtokens.get_char('#filename'), "Should print out some sort of file");
     }
 
     @org.junit.jupiter.api.Test
     void unget_char() {
+        Printtokens t = new Printtokens();
+        assertEquals(0,t.unget_char(97,t.open_token_stream(null)));
     }
 
     @org.junit.jupiter.api.Test
-    void open_token_stream() {
+    void open_token_stream_txtfile() throws FileNotFoundException {
+        Printtokens t = new Printtokens();
+        assertNotNull(t.open_token_stream("testfile1.txt"),"BufferedReader should not be null");
+    }
+
+    @org.junit.jupiter.api.Test
+    void open_token_stream_nullfile() throws FileNotFoundException {
+        Printtokens t = new Printtokens();
+        assertNotNull(t.open_token_stream(null),"BufferedReader should not be null");
     }
 
     @org.junit.jupiter.api.Test
     void get_token() {
-        int i = 0, j;
-        int id = 0;
-        int res = 0;
-        char ch = '\0';
-        BufferedReader br = new BufferedReader(new StringReader(input));
-        Printtokens t = new Printtokens();
-
-        assertEquals(-1, res, "return NULL");
-        assertEquals('\0', ch, "return NULL");
-        assertEquals(0, id  , "return NULL");
-
-        assertEquals(40, res, "return (");
-        assertEquals('(', ch, "return (");
-        assertEquals(0, id  , "return (");
-
-
-        assertNull(t.get_token(br), "After it's read through, it should return NULL at the end, noting EOF");
+        //complicated method 5
+    }
 
     @org.junit.jupiter.api.Test
     void is_token_end() {
     }
 
     @org.junit.jupiter.api.Test
-    void token_type() {
+    void token_type_keyword() {
+        String tok = "and";
+        assertEquals(1,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_spec_symbol() {
+        String tok = "(";
+        assertEquals(2,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_identifier() {
+        String tok = "aa";
+        assertEquals(3,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_num_constant() {
+        String tok = "123";
+        assertEquals(41,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_str_constant() {
+        String tok = "\"a3\"";
+        assertEquals(42,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_char_constant() {
+        String tok = "#a";
+        assertEquals(43,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_comment() {
+        String tok = ";test";
+        assertEquals(5,Printtokens.token_type(tok));
+    }
+    @org.junit.jupiter.api.Test
+    void token_type_error() {
+        String tok = null;
+        assertEquals(0,Printtokens.token_type(tok));
     }
 
     @org.junit.jupiter.api.Test
-    void print_token() {
+    void print_token_error() {
+        String tok = null;
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("error,\"" + tok + "\".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_keyword() {
+        String tok = "and";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("keyword,\"" + tok + "\".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_spec_symbol() {
+        String tok = "(";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("lparen.\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_identifier() {
+        String tok = "aa";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("identifier,\"" + tok + "\".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_num_constant() {
+        String tok = "123";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("numeric," + tok + ".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_str_constant() {
+        String tok = "\"a3\"";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("string," + tok + ".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_char_constant() {
+        String tok = "#a";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("character,\"" + tok.charAt(1) + "\".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+    }
+    @org.junit.jupiter.api.Test
+    void print_token_comment() {
+        String tok = ";test";
+        Printtokens t = new Printtokens();
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        t.print_token(tok);
+        assertEquals("comment,\"" + tok + "\".\n", outContent.toString());
+        System.setOut(System.out); //clears system.out
+
     }
 
     @org.junit.jupiter.api.Test
-    void is_comment() {
+    void is_comment_true() {
+        assertTrue(Printtokens.is_comment(";test"));
+    }
+    @org.junit.jupiter.api.Test
+    void is_comment_false() {
+        assertFalse(Printtokens.is_comment("test"));
     }
 
     @org.junit.jupiter.api.Test
-    void is_keyword() {
+    void is_keyword_true() {
+        assertTrue(Printtokens.is_keyword("and"));
+    }
+    @org.junit.jupiter.api.Test
+    void is_keyword_false() {
+        assertFalse(Printtokens.is_keyword("test"));
     }
 
     @org.junit.jupiter.api.Test
-    void is_char_constant() {
+    void is_char_constant_true() {
+        assertTrue(Printtokens.is_keyword("#a"));
+    }
+    @org.junit.jupiter.api.Test
+    void is_char_constant_false() {
+        assertFalse(Printtokens.is_keyword("#3"));
     }
 
     @org.junit.jupiter.api.Test
     void is_num_constant() {
-        var numConstant = new Printtokens();
-        String str1 = null;
-        String str2 = '#ab';
-        String str3 = '123';
-        String str4 = '12a3';
-        assertFalse(numConstant.is_num_constant(str1));
-        assertFalse(numConstant.is_num_constant(str2));
-        assertTrue(numConstant.is_num_constant(str3));
-        assertFalse(numConstant.is_num_constant(str4));
-
     }
 
     @org.junit.jupiter.api.Test
     void is_str_constant() {
-        var strConstant = new Printtokens();
-        String str1 = 'abs';
-        String str2 = '""';
-        String str3 = '"The fox"';
-        String str4 = '"No quotes on the right';
-        String str5 = ',';
-        assertFalse(strConstant.is_str_constant(str1));
-        assertTrue(strConstant.is_str_constant(str2));
-        assertTrue(strConstant.is_str_constant(str3));
-        assertFalse(strConstant.is_str_constant(str4));
-        assertFalse(strConstant.is_str_constant(str5));
-
     }
 
     @org.junit.jupiter.api.Test
     void is_identifier() {
-        assertTrue(Printtokens.is_identifier("identifier"), "'identifier' should be an identifier");
-        assertFalse(Printtokens.is_identifier("123"), "'123' should not be an identifier");
     }
 
     @org.junit.jupiter.api.Test
     void print_spec_symbol() {
-        Printtokens.print_spec_symbol("(");
-        Printtokens.print_spec_symbol(")");
-        Printtokens.print_spec_symbol("[");
-        Printtokens.print_spec_symbol("]");
-        Printtokens.print_spec_symbol("'");
-        Printtokens.print_spec_symbol("`");
-        Printtokens.print_spec_symbol("(");
     }
 
     @org.junit.jupiter.api.Test
     void is_spec_symbol() {
-        assertTrue(Printtokens.is_spec_symbol('('), " '(' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol(')'), " ')' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol('['), " '[' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol(']'), " ']' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol('/'), " '/' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol('`'), " '`' should be a special symbol");
-        assertTrue(Printtokens.is_spec_symbol(','), " ',' should be a special symbol");
-        assertFalse(Printtokens.is_spec_symbol('#'), " '#' should not be a special symbol");
-
-
-
     }
 
     @org.junit.jupiter.api.Test
     void main() {
-        var main = new Printtokens();
-        assertFalse(Printtokens.main(null), "Should print out the statement given");
-        assertTrue(Printtokens.main(""), "Should exit out of the system");
-        assertTrue(Printtokens.main('a'), "Should exit out of the system");
-        assertTrue(Printtokens.main('#filename'), "Should exit out of the system");
     }
 }
